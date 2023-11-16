74c5ed8b4a954ca7396c5eebe1248c42267bad23
MzubairSultan <mz573206@gmail.com>
2023-11-16 23:53:11 +0500

    first

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,7 +6,7 @@ import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
 import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
-import Resume from './resumenhrnce.pdf';
+import Resume from './NewZubair.pdf';
 
 const Services = () => {
   // context
